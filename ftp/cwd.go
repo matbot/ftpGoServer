d0b451fd130425dff611aad0ff931de5adc0273d
matbotW <mathewmcdade@gmail.com>
2020-06-15 09:11:49 -0700

    basic cwd implemented

--- ftp/cwd.go
+++ ftp/cwd.go
@@ -1,1 +1,25 @@
 package ftp
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+// TODO: Improve implementation of dir handling and implement path permissions.
+func (c *Connection) cwd(args []string) {
+	if len(args) != 1 {
+		c.respond(status501)
+		return
+	}
+	workingDir := filepath.Join(c.workingDir, args[0])
+	absPath := filepath.Join(c.rootDir, workingDir)
+	_, err := os.Stat(absPath)
+	if err != nil {
+		log.Print(err)
+		c.respond(status550)
+		return
+	}
+	c.workingDir = workingDir
+	c.respond(status220)
+}
--- ftp/user.go
+++ ftp/user.go
@@ -1,1 +1,11 @@
 package ftp
+
+import (
+	"fmt"
+	"strings"
+)
+
+// TODO: enhance user functionality with authentication and permissions.
+func (c *Connection) user(args []string) {
+	c.respond(fmt.Sprintf(status230, strings.Join(args, " ")))
+}
